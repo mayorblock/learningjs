6e8d8502c7fd73e0426d7aaf547fe975a7921659
mikael <sonnehansen@gmail.com>
2024-05-15 20:35:54 +0200

    enable CORS

--- dummyapi/main.py
+++ dummyapi/main.py
@@ -1,9 +1,20 @@
 import random
 from fastapi import FastAPI
 from datetime import datetime, timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
