8d17cc54c1815f889323b54c15e7221171c6f2c5
mikael <sonnehansen@gmail.com>
2024-05-15 20:36:37 +0200

    plotly.js using data from dummyapi

--- webpage/subpages/script.js
+++ webpage/subpages/script.js
@@ -1,1 +1,15 @@
-alert("Hello, World!");
+$("button").click(function(){
+    $.get("http://127.0.0.1:8000/temperature", function(data, status){
+      console.log(data);
+      console.log(status)
+
+      var trace = {
+        x: data["dates"],
+        y: data["temperature"],
+        type: 'scatter'
+      };
+      
+     Plotly.newPlot('tester', [trace]);
+    });
+  });
+
