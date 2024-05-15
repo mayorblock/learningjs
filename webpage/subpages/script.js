ed1d4ba76ef5387b9b7b88cf26cf3cf263af7731
mikael <sonnehansen@gmail.com>
2024-05-15 20:48:09 +0200

    plotly title and such

--- webpage/subpages/script.js
+++ webpage/subpages/script.js
@@ -1,3 +1,37 @@
+
+var layout = {
+    title: {
+      text:'Vandtemperatur',
+      font: {
+        family: 'Courier New, monospace',
+        size: 24
+      },
+      xref: 'paper',
+      x: 0.05,
+    },
+    xaxis: {
+      title: {
+        text: 'Dato',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Temperatur [&#8451;]',   // degree celcius unicode
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+
+
 $("button").click(function(){
     $.get("http://127.0.0.1:8000/temperature", function(data, status){
       console.log(data);
@@ -9,7 +43,7 @@ $("button").click(function(){
         type: 'scatter'
       };
       
-     Plotly.newPlot('tester', [trace]);
+     Plotly.newPlot('tester', [trace], layout);
     });
   });
 
